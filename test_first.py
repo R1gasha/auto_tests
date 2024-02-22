e71c7598880148978e86d55920edd68f3b77e03a
Igor Chuchkalov <chuchkalov@dtbyh.ru>
2024-02-22 12:08:30 +0700

    add tests

--- test_first.py
+++ test_first.py
@@ -1,23 +1,32 @@
-import time
 from .pages.main_page import MainPage
 from .pages.tensor_page import TensorPage
 from .pages.contact_page import ContactPage
 from .pages.about_page import AboutPage
 
 
-def test_first(browser):
+class TestFirst:
     link = 'https://sbis.ru/'
-    page_main = MainPage(browser, link)
-    page_main.open()
-    page_main.go_to_contacts()
-    
-    page_contact = ContactPage(browser, browser.current_url)
-    page_contact.go_to_tensor()
+    about_url = 'https://tensor.ru/about'
 
-    tesor_page = TensorPage(browser, browser.current_url)
-    tesor_page.should_be_block_people()
-    tesor_page.go_to_about()
+    def test_go_to_contacts(self, browser):
+        page_main = MainPage(browser, self.link)
+        page_main.open()
+        page_main.go_to_contacts()
 
-    about_page = AboutPage(browser, browser.current_url)
-    about_page.should_be_currect_url('https://tensor.ru/about')
-    about_page.should_be_currect_size()
+    def test_go_to_tensor(self, browser):
+        page_contact = ContactPage(browser, browser.current_url)
+        page_contact.go_to_tensor()
+
+    def test_check_block(self, browser):
+        tesor_page = TensorPage(browser, browser.current_url)
+        tesor_page.should_be_block_people()
+        
+
+    def test_go_to_about(self, browser):
+        tesor_page = TensorPage(browser, browser.current_url)
+        tesor_page.go_to_about()
+
+    def test_check_url_size(self, browser):
+        about_page = AboutPage(browser, browser.current_url)
+        about_page.should_be_currect_url(self.about_url)
+        about_page.should_be_currect_size()
--- test_second.py
+++ test_second.py
@@ -1,23 +1,32 @@
-import time
 from .pages.main_page import MainPage
 from .pages.tensor_page import TensorPage
 from .pages.contact_page import ContactPage
 from .pages.about_page import AboutPage
 
-def test_second(browser):
+class TestSecond:
     link = 'https://sbis.ru/'
     old_region = 'Новосибирск'
     new_region = 'Камчатский край'
     change_url = '41-kamchatskij-kraj'
-    page_main = MainPage(browser, link)
-    page_main.open()
-    page_main.go_to_contacts()
-    
-    page_contact = ContactPage(browser, browser.current_url)
-    page_contact.check_region(old_region)
-    page_contact.change_region(new_region)
 
-    page_contact.check_region(new_region)
-    page_contact.check_title_region(new_region)
-    page_contact.check_url_region(change_url)
+    def test_go_to_contacts(self, browser):
+        page_main = MainPage(browser, self.link)
+        page_main.open()
+        page_main.go_to_contacts()
+
+    def test_check_region(self, browser):
+        page_contact = ContactPage(browser, browser.current_url)
+        page_contact.check_region(self.old_region)
+        
+
+    def test_change_region(self, browser):
+        page_contact = ContactPage(browser, browser.current_url)
+        page_contact.change_region(self.new_region)
+
+    def test_chech_new_region(self, browser):
+        page_contact = ContactPage(browser, browser.current_url)
+        page_contact.check_region(self.new_region)
+        page_contact.check_title_region(self.new_region)
+        page_contact.check_url_region(self.change_url)
+
 
